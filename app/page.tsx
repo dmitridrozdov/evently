eb4b9716b57a0827214da9646689a548ec7e677c
dmitridrozdov <dm.drozdov@gmail.com>
2023-12-22 09:09:14 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      <Button variant='ghost'>start</Button>
+      <Button variant='destructive' className="px-10">start</Button>
     </main>
   )
 }
