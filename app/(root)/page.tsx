94def2c9fa18b676fd9ac388d0b604785428bf6a
dmitridrozdov <dm.drozdov@gmail.com>
2023-12-22 14:28:19 +1100

    Create page.tsx

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <Button variant='destructive' className="px-10">moved page</Button>
+    </main>
+  )
+}
